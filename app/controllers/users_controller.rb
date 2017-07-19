6b07ec41ea46655f35331325ec7b204b8b59bd6f
Evan Hickle & Nico Glennon <pair+ehickle.nicoglennon@devbootcamp.com>
2017-07-19 15:46:15 -0500

    Add create and user_params methods to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      puts "It saved!!"
+      redirect_to new_session_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
+  get '/sessions/new', to: 'sessions:new', as: 'new_session'
+  resources :users
 end
