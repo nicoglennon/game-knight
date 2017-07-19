27bca6eaa1704ee8341085ba3525239bb5f4d1ff
Chris McMenomy & Kristina Vragovic <pair+caristopmer.kvrag@devbootcamp.com>
2017-07-19 16:21:45 -0500

    Basic home index page created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+
+  get '/' => 'pages#index'
   resources :games, only: [:show, :index]
   resources :categories, only: [:show]
   resources :mechanisms, only: [:show]
