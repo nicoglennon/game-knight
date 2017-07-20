1c13ac875ec36552c98b95edb7dca9e963b78fc0
Chris McMenomy <email": null>
2017-07-20 15:07:50 -0500

    in progress save point

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,8 @@ require 'date'
 require 'net/http'
 require 'json'
 
-# Comment the methods below in or out depending on what you want to do.
-seed_test_data
-seed_games
-
 # Method to seed fake data for app testing
-def seed_test_data
+def seed_user_data
   tom = User.new(username: "tomtom", email: "tom@tom.com")
   tom.password = "tomtom"
   tom.save
@@ -16,8 +12,9 @@ def seed_test_data
   tim = User.new(username: "timtim", email: "tim@tim.com")
   tim.password = "timtim"
   tim.save
+end
 
-
+def seed_test_game_data
   player_numbers = ["2-4", "2-6", "2", "4-8"]
   durationz = ["30-60", "120+", "60-90"]
   release_dates = ["2012", "1995", "2005", "2016"]
@@ -28,7 +25,9 @@ def seed_test_data
               number_of_players: player_numbers.sample,
               duration: durationz.sample,
               release_date: release_dates.sample,
-              publisher: Faker::Zelda.character + " Games"
+              publisher: Faker::Zelda.character + " Games",
+              image_url: "",
+              image_thumbnail_url: ""
               )
   end
 
@@ -43,7 +42,9 @@ def seed_test_data
   10.times do
     GameCategory.create(game_id: Game.all.sample.id, category_id: Category.all.sample.id)
   end
+end
 
+def seed_review_data
   15.times do
     Review.create(
       title: Faker::MostInterestingManInTheWorld.quote,
@@ -55,17 +56,12 @@ end
 
 
 # Method to seed basic game data
-def seed_games
+def seed_real_games
   uri = URI("https://bgg-json.azurewebsites.net/collection/caristopmer")
 
   json_string = Net::HTTP.get(uri)
 
   collection_array = JSON.parse(json_string)
-
-  def player_range(game)
-    "#{game["maxPlayers"]}" if game["minPlayers"] == game["maxPlayers"]
-    "#{game["minPlayers"]}-#{game["maxPlayers"]}"
-  end
 
   game_params_array = []
   collection_array.each do |game|
@@ -82,13 +78,59 @@ def seed_games
 
   temp_games = []
   game_params_array.each do |params|
-    temp_games << Game.new(params)
+    temp_games << Game.create(params)
+  end
+end
+
+def seed_desc_mechs_designers
+  games = Game.all
+  game_ids = []
+  games.each do |game|
+    game_ids << game.bgg_id
   end
 
-  p temp_games
+  game_ids.each do |game_id|
+    uri = URI("https://bgg-json.azurewebsites.net/thing/#{game_id}")
+    info_hash = JSON.parse(Net::HTTP.get(uri))
+
+    current_game = Game.find_by(bgg_id: game_id)
+    current_game[:description] = info_hash["description"]
+
+
+
+    wait 5 seconds
+  end
+
+
+  game_params_array = []
+  collection_array.each do |game|
+    game_args = {}
+    game_args[:title] = game["name"]
+    game_args[:number_of_players] = player_range(game)
+    game_args[:duration] = game["playingTime"]
+    game_args[:release_date] = game["yearPublished"].to_s
+    game_args[:image_url] = game["image"]
+    game_args[:image_thumbnail_url] = game["thumbnail"]
+    game_args[:bgg_id] = game["gameId"]
+    game_params_array << game_args
+  end
+
+  temp_games = []
+  game_params_array.each do |params|
+    temp_games << Game.create(params)
+  end
 end
 
 def player_range(game)
-  "#{game["maxPlayers"]}" if game["minPlayers"] == game["maxPlayers"]
+  return "#{game["maxPlayers"]}" if game["minPlayers"] == game["maxPlayers"]
   "#{game["minPlayers"]}-#{game["maxPlayers"]}"
 end
+
+#************************************
+# Comment the methods below in or out depending on what you want to do.
+ seed_user_data
+ seed_test_game_data
+ seed_review_data
+ seed_real_games
+# seed_desc_mechs_designers
+#************************************
