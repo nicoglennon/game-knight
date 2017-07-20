c63b563a975e4c64ad656f40cf8565a3ea4d1063
Chris McMenomy <email": null>
2017-07-20 15:44:42 -0500

    getting categories will be tough, progress in game-grabber.rb file.

--- db/game-grabber.rb
+++ db/game-grabber.rb
@@ -1,35 +1,13 @@
 require 'net/http'
 require 'json'
+require 'nokogiri'
 
-uri = URI("https://bgg-json.azurewebsites.net/collection/caristopmer")
+uri = URI("https://boardgamegeek.com/xmlapi2/thing?id=169786")
 
-json_string = Net::HTTP.get(uri)
+xml_string = Net::HTTP.get(uri)
 
-collection_array = JSON.parse(json_string)
+game_info = Nokogiri::XML(xml_string)
 
-def player_range(game)
-  "#{game["maxPlayers"]}" if game["minPlayers"] == game["maxPlayers"]
-  "#{game["minPlayers"]}-#{game["maxPlayers"]}"
-end
+# p game_info.css("items link").attr('value').value
 
-game_params_array = []
-collection_array.each do |game|
-  game_args = {}
-  game_args[:title] = game["name"]
-  game_args[:number_of_players] = player_range(game)
-  game_args[:duration] = game["playingTime"]
-  game_args[:release_date] = game["yearPublished"].to_s
-  game_args[:image_url] = game["image"]
-  game_args[:image_thumbnail_url] = game["thumbnail"]
-  game_args[:bgg_id] = game["gameId"]
-  game_params_array << game_args
-end
-
-p game_params_array
-
-temp_games = []
-game_params_array.each do |params|
-  temp_games << Game.new(params)
-end
-
-p temp_games
+p game_info.css("items link").attr('value').value
