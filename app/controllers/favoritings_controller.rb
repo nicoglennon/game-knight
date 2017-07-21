35c15541fd28c0a3301dff81ed9c09b4194f25e7
Evan Hickle <ehickle@alumni.nd.edu>
2017-07-20 20:43:02 -0500

    Make buttons for adding and favoriting a game into forms. Effectively creates a favoriting or ownership and redirects back to current page. No error handling

--- app/controllers/favoritings_controller.rb
+++ app/controllers/favoritings_controller.rb
@@ -1,10 +1,13 @@
 class FavoritingsController < ApplicationController
 
   def create
+    p params
     @favoriting = Favoriting.new(user_id: current_user.id, game_id: params[:favoriting][:game])
     if @favoriting.save
+      redirect_to params[:favoriting][:path]
     else
       @errors = @favoriting.errors.full_messages
+      redirect_to params[:favoriting][:path]
     end
 
   end
--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -2,8 +2,10 @@ class OwnershipsController < ApplicationController
     def create
     @ownership = Ownership.new(user_id: current_user.id, game_id: params[:ownership][:game])
     if @ownership.save
+      redirect_to params[:ownership][:path]
     else
-      @errors = @ownership.errors.full_messages
+      flash[:notice] = "This game is already in your collection"
+      redirect_to params[:ownership][:path], flash: {error: @ownership.errors.full_messages}
     end
 
   end
