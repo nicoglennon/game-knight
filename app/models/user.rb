663480c0aee9e833d68fa6849e62f99013a1571a
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-21 11:31:59 -0500

    Add dat Paperclip back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :reviews, foreign_key: :author_id
 
   validates :email, :username, presence:true, uniqueness:true
+
+#PAPERCLIP
+  attr_accessible :avatar
+  has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }, default_url: 'http://s3.amazonaws.com/game-knight-user-avatars/default-avatar.jpg'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Paperclip and s3 configuration
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('AWS_AVATAR_BUCKET'),
+      access_key_id: ENV.fetch('AWS_ACCESS'),
+      secret_access_key: ENV.fetch('AWS_SECRET'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
