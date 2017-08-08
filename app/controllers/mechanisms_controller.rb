f0d36bf1f84e3ce9b770aca8911b5dcd35e09d07
Nico Glennon <nicoglennon@icloud.com>
2017-08-08 11:47:39 -0500

    Fix card where mechanisms and categories are displayed in the game#show page. Also fixed Mechanism & Categories pages

--- app/controllers/mechanisms_controller.rb
+++ app/controllers/mechanisms_controller.rb
@@ -2,5 +2,7 @@ class MechanismsController < ApplicationController
 
   def show
     @mechanism = Mechanism.find(params[:id])
+    @favoriting = Favoriting.new
+    @ownership = Ownership.new
   end
 end
