e8a73dae26570242e3942f89df9d01d34ecefed5
Nico Glennon <nicoglennon@icloud.com>
2017-08-08 11:13:45 -0500

    Flash alert notifying user that game was favorited / added is now inside a card div, making it easy to see

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -2,10 +2,10 @@ class OwnershipsController < ApplicationController
     def create
     @ownership = Ownership.new(user_id: current_user.id, game_id: params[:ownership][:game])
     if @ownership.save
-      flash[:notice] ='Game added to your collection'
+      flash[:notice] ="'#{@ownership.game.title}' was added to your collection."
       redirect_to params[:ownership][:path]
     else
-      flash[:notice] = "This game is already in your collection"
+      flash[:notice] = "'#{@ownership.game.title}' is already in your collection!"
       redirect_to params[:ownership][:path]
     end
 
